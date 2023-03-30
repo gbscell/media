278c2e30757bce40f0e51dc97b99d20bb1ff4c4d
gbscell <120020324+gbscell@users.noreply.github.com>
2023-03-30 15:54:55 +0700

    Update aipitv.php

--- aipitv.php
+++ aipitv.php
@@ -1,16 +1,13 @@
 #EXTM3U
 
 #EXTINF:-1 tvg-logo="" group-title="DISCLAIMER & INFO",DISCLAIMER
-https://aipitv.my.id/vod/disclaimer.mp4
+https://github.com/gbscell/media/blob/main/disclaimer.mp4?raw=true
 
 #EXTINF:-1 tvg-logo="" group-title="DISCLAIMER & INFO",FAQ
-https://aipitv.my.id/vod/faq.mp4
-
-#EXTINF:-1 tvg-logo="" group-title="DISCLAIMER & INFO",ORANG BAIK
-#https://aipitv.my.id/vod/orangbaik.mp4
+https://github.com/gbscell/media/blob/main/faq.mp4?raw=true
 
 #EXTINF:-1 tvg-logo="" group-title="DISCLAIMER & INFO",DONASI
-https://aipitv.my.id/vod/donasi.mp4
+https://github.com/gbscell/media/blob/main/donasi.mp4?raw=true
 
 #EXTINF:-1 tvg-logo="" group-title="DISCLAIMER & INFO",TENTANG
-https://aipitv.my.id/vod/about355.mp4
+https://github.com/gbscell/media/blob/main/about.mp4?raw=true

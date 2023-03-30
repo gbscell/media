bd92fb8749cefbc3d4e6e2f91aa20324ae4abe43
gbscell <120020324+gbscell@users.noreply.github.com>
2023-03-30 15:30:20 +0700

    Create aipitv.php

---
+++ aipitv.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <script language="javascript">
+    window.location = "https://aipitv.my.id";
+  </script>
+</head>
+<body>
+#EXTM3U url-tvg="https://iptv-org.github.io/epg/guides/id-id/mncvision.id.epg.xml.gz" refresh="3600"
+
+<script type="text/javascript">
+
+#EXTINF:-1 group-title="INFO GANGGUAN",TOLONG DIBACA
+#https://aipitv.my.id/vod/gangguan_sport.mp4
+
+#EXTINF:-1 group-title="HATI-HATI DENGAN PENJUAL ILEGAL",TOLONG DIBACA
+#https://aipitv.my.id/vod/pencuriapk.mp4
+
+#EXTINF:-1 tvg-logo="https://aipitv.my.id/tvlogo/live/defaultlogo.png" group-title="DISCLAIMER & INFO",DISCLAIMER
+https://aipitv.my.id/vod/disclaimer.mp4
+
+#EXTINF:-1 tvg-logo="https://aipitv.my.id/tvlogo/live/defaultlogo.png" group-title="DISCLAIMER & INFO",FAQ
+https://aipitv.my.id/vod/faq.mp4
+
+#EXTINF:-1 tvg-logo="https://aipitv.my.id/tvlogo/live/defaultlogo.png" group-title="DISCLAIMER & INFO",ORANG BAIK
+#https://aipitv.my.id/vod/orangbaik.mp4
+
+#EXTINF:-1 tvg-logo="https://aipitv.my.id/tvlogo/live/defaultlogo.png" group-title="DISCLAIMER & INFO",DONASI
+https://aipitv.my.id/vod/donasi.mp4
+
+#EXTINF:-1 tvg-logo="https://aipitv.my.id/tvlogo/live/defaultlogo.png" group-title="DISCLAIMER & INFO",TENTANG
+https://aipitv.my.id/vod/about352.mp4
+  
+</script>
+</body>
+</html>
